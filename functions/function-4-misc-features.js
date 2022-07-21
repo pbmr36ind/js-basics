6b90f0242e175d744348c06e7f902f6441fae39b
pbmr36.ind <pbmr36.ind@gmail.com>
2022-07-21 17:28:00 -0400

    more on spreading

--- functions/function-4-misc-features.js
+++ functions/function-4-misc-features.js
@@ -61,7 +61,9 @@ blah();
 
 // SPREAD
 
-// spread in function calls 
+// # 1 : SPREAD in function calls 
+
+/*
 
 console.log('Max fn pass values : ', Math.max(1,2,3,4,5,6));
 
@@ -71,5 +73,75 @@ console.log('Max fn pass array : ', Math.max(nums));
 
 console.log('Max fn spread array : ', Math.max(...nums));
 
+*/
+
+/*
+
+function giveMeFour(a, b, c, d) {
+  console.log('a:', a);
+  console.log('b:', b);
+  console.log('c:', c);
+  console.log('d:', d);
+}
+
+//const colors = ['red', 'green', 'blue', 'cyan'];
+
+const str1 = 'GOAT';
+
+giveMeFour(...str1);
+
+*/
+
+
+// # 2 : SPREAD in Array Literals.
+
+/*
+const num1 = [12, 20, 32];
+const num2 = [10, 11, 12];
+
+const nums = ['a', 'b', ...num1, ...num2  ];
+
+console.log(nums);
+
+const num1Copy = [...num1];
+
+console.log(num1Copy);
+
+console.log(num1 === num1Copy);
+
+// we can spread strings
+
+const str1Array = [..."GOAT", ..."IE"];
+
+console.log(str1Array);
+
+*/
+
+// # 3 : SPREAD in Objects.
+
+const car1 = {
+  name  : "Camry",
+  brand : "Toyota",
+  trim  : "LE"
+}
+
+
+const camryLE1 = {
+  ...car1,
+  mileage : 5200,
+  price : 20450,
+  color : 'Cystal Blue'
+}
+
+const camryLE2 = {
+  ...camryLE1
+}
+
+
+const cars = ['camry', 'accord', 'civic', 'cx-5'];
+
+const carsArr = [...cars, {..."pr"} ];
+
+console.log(carsArr);
 
 
